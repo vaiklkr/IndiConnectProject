715bb8995155b050235bfd03e4ad3c7ae84ddb08
vaiklkr <104137188+vaiklkr@users.noreply.github.com>
2023-03-14 11:21:21 +0530

    Delete indiconnect extra folder

--- indiConnect/backend/myproj15/CustomerApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from CustomerApp.models import Cust
-# Create your views here.
-
-def cust_input(request):
-    custs = Cust.objects.all()
-    return render(request,'base.html',{'cust_d':custs})
--- indiConnect/backend/myproj15/myproj15/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path,re_path
-from CustomerApp import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^CustomerApp$',views.cust_input)
-]
